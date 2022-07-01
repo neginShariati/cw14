67a23757af365f319be2c1b41026f679404eb1e0
khadije <khadijeh.allahgholi@gmail.com>
2022-07-01 16:21:46 +0430

    exercise1 added

--- app.js
+++ app.js
@@ -2,23 +2,28 @@ const API_URL = 'https://62b397444f851f87f45d24a3.mockapi.io';
 
 $(document).ready(function(){
     const list = $("table tbody");
-    $.ajax({url: `${API_URL}/toDoList`}).done(function(users){
-        $.each(users , function(index , user){
-            list.append(`<tr><td>${user.list}</td><tr>`)
+    function getData(){
+        $.ajax({url: `${API_URL}/toDoList`}).done(function(users){
+            $.each(users , function(index , user){
+                list.append(`<tr><td>${user.list}</td><tr>`)
+            });
         });
+    }
+    $("#btn-addItem").click(function(){
+        let inputValue = $("input").val();
+        $.ajax({
+            url: `${API_URL}/toDoList`,
+            type: 'POST',
+            data: JSON.stringify({ list: inputValue}),
+            contentType: 'application/json; charset=utf-8',
+            success: function (data) {
+                list.append(`<tr><td>${data.list}</td><tr>`)
+            },
+          });
+        console.log(inputValue);
     });
+    getData();
 });
 
-$("#btn-addItem").click(createItem);
 
-function createItem(){
-    $.ajax({
-        url: `${API_URL}/toDoList`,
-        type: 'POST',
-        data: JSON.stringify({ list: $("input").val() }),
-        contentType: 'application/json; charset=utf-8',
-        success: function (data) {
-            list.append(`<tr><td>${data}</td><tr>`)
-        },
-      });
-}
+
